0ff18a9550762686a0b6b09c0df26a090d74162b
Yashan Thilakasiri <52060474+YashanThilakasiriMMYS@users.noreply.github.com>
2020-08-16 14:04:28 +0530

    bubble sort algorithm implementation

---
+++ 5.sortingAlgorithms/bubbleSort.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+
+void bubbleSort(int a[])
+{
+    for (int i = 0; i < 5; i++)
+    {
+        for (int j = 0; j < (5 - i - 1); j++)
+        {
+            if (a[j] > a[j + 1])
+            {
+                int temp = a[j];
+                a[j] = a[j + 1];
+                a[j + 1] = temp;
+            }
+        }
+    }
+};
+
+int main()
+{
+    int arr[5];
+    cout << "Enter 5 numbers in random order: " << endl;
+    for (int i = 0; i < 5; i++)
+    {
+        cin >> arr[i];
+    }
+    cout << "Before Sorting : " << endl;
+    for (int i = 0; i < 5; i++)
+    {
+        cout << arr[i] << "  ";
+    }
+    cout << endl;
+
+    bubbleSort(arr);
+
+    cout << "After Sorting : " << endl;
+    for (int i = 0; i < 5; i++)
+    {
+        cout << arr[i] << "  ";
+    }
+    return 0;
+}
